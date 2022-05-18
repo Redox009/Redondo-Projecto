7158381f344201b65bb26b469f6de0a3ed64c980
Redox009 <97345898+Redox009@users.noreply.github.com>
2022-05-18 19:51:50 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -2,26 +2,90 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
+  
 
-app.get('/', (req, res ) => {
-    res.send('Hola Mundo')
-})
-
-app.get('/productos', (req, res ) => {
-    let data = fs.readFileSync('./productos.txt')
-    data = JSON.parse(data)
-    res.send(data)
-})
-
-app.get('/productosRandom', (req, res ) => {
-    let data = fs.readFileSync('./productos.txt')
-    var dataR = data[Math.floor(Math.random() * data.length)];
-    res.send( dataR )
-})
-
-
+app.get('/productos', (req, res) => {
+    console.log('Request recibido')
+    
+    if (!req.query.titulo) {
+      return res.json(productos)
+    }
+  
+    const tituloFiltered = productos.filter(productos => productos.titulo === req.query.titulo)
+  
+    return res.json(tituloFiltered)
+  })
+  
+  app.get('/productos/:id', (req, res) => {
+    console.log('Request recibido')
+  
+    const id = Number(req.params.id)
+    const producto = productos.find(productos => productos.id === id)
+  
+    if (!producto) {
+      return res.status(404).json({
+        error: 'Mensaje no encontrado'
+      })
+    }
+  
+    return res.json(producto)
+  })
+  
+  app.post('/productos', (req, res) => {
+    console.log('POST request recibido')
+    console.log({ body: req.body })
+  
+    const newProducto = req.body
+  
+    newProducto.id = productos.length + 1
+  
+    productos.push(newProducto)
+  
+    return res.status(201).json(newProducto)
+  })
+  
+  app.put('/productos/:id', (req, res) => {
+    console.log('PUT request recibido')
+  
+    const id = Number(req.params.id)
+    const productoIndex = productos.findIndex(productos => productos.id === id)
+  
+    if (productoIndex === -1) {
+      return res.status(404).json({
+        error: 'Mensaje no encontrado'
+      })
+    }
+  
+    const body = req.body
+  
+    productos[productoIndex].titulo = body.titulo
+    productos[productoIndex].descripcion = body.descripcion
+  
+  
+    return res.json(messages[messageIndex])
+  })
+  
+  app.delete('/productos/:id', (req, res) => {
+    console.log('DELETE request recibido')
+  
+    const id = Number(req.params.id)
+    const productoIndex = productos.findIndex(productos => productos.id === id)
+  
+    if (messageIndex === -1) {
+      return res.status(404).json({
+        error: 'Mensaje no encontrado'
+      })
+    }
+  
+    productos = productos.filter(productos => productos.id !== id)
+  
+    return res.status(204).json({})
+  })
+  
 
 
 
